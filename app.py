f5fac3fc59b0605a6670ab82172a707bf9c5adf6
rashidpatel98677-sketch <rashidpatel98677@gmail.com>
2026-04-09 11:07:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 import joblib
+import os
 
 app = Flask(__name__)
 
@@ -55,4 +56,5 @@ def predict_waterQ():
         return f"<h3>Error:</h3><p>{e}</p>"
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
